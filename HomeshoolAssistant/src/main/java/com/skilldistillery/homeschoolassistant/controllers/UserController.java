81370b41aa964f955da66da5bad981df2cdd0567
VulcanMindForge <jastuart@live.com>
2023-12-08 10:33:44 -0700

    Ready for pull

--- HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/controllers/UserController.java
+++ HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/controllers/UserController.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.homeschoolassistant.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.skilldistillery.homeschoolassistant.data.UserDAO;
+
+@Controller
 public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
 
 }
--- HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/data/UserDaoImpl.java
+++ HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/data/UserDaoImpl.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.homeschoolassistant.data;
 
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class UserDaoImpl implements UserDAO {
 
+	@PersistenceContext
+	EntityManager em;
 }
