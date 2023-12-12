66b55ae0deb038bdf80e9d12462732dfe1afcd3b
VulcanMindForge <jastuart@live.com>
2023-12-12 08:44:04 -0700

    ADO implemented for assignment, lessonplan, and resource. updated resource and lessonplan with timestamp annotation.

--- HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/data/AssignmentDAO.java
+++ HomeshoolAssistant/src/main/java/com/skilldistillery/homeschoolassistant/data/AssignmentDAO.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.homeschoolassistant.data;
 
-public interface AssignmentDAO {
+import com.skilldistillery.homeschoolassistant.entities.Assignment;
+import com.skilldistillery.homeschoolassistant.entities.LessonPlan;
+import com.skilldistillery.homeschoolassistant.entities.Resource;
 
+public interface AssignmentDAO {
+	Resource addResource(Resource resource);
+	Resource modifyResource(int resourceId, Resource resource);
+	Boolean removeResourece(int resourceId);
+	
+	Assignment addAssignment(Assignment assignment);
+	Assignment modifyAssignment(int assignmentId, Assignment assignment);
+	Boolean removeAssignment(int assignmentId);
+	
+	LessonPlan addLessonPlan(LessonPlan lessonPlan);
+	LessonPlan modifyLessonPlan(int lessonPlanId, LessonPlan lessonPlan);
+	Boolean removeLessonPlan(int lessonPlanId);
 }
